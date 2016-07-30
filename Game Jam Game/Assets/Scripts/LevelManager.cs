51bb4f1d67b3111ea136979f2bc6d11d81ae02d0
elijahcauley <elijah.cauley@lawrence.edu>
2016-07-30 17:59:05 -0400

    Added menu system skeleton.

---
+++ Game Jam Game/Assets/Scripts/LevelManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LevelManager : MonoBehaviour {
+	
+	public float autoLoadNextLevelAfter;
+	
+	void Start () {
+		if (autoLoadNextLevelAfter == 0) {
+			Debug.Log ("Level auto load disabled");
+		} else {
+			Invoke ("LoadNextLevel", autoLoadNextLevelAfter);
+		}
+	}
+
+	public void LoadLevel (string name) {
+		Debug.Log("New Level Load: " + name);
+		Application.LoadLevel(name);
+	}
+	
+	public void QuitRequest() {
+		Debug.Log ("Quit Requested");
+		Application.Quit();
+	}
+	
+	public void LoadNextLevel () {
+		Application.LoadLevel(Application.loadedLevel + 1);
+	}
+	
+	
+}
